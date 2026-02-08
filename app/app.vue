6d8af2995754ef4cd772faa90d094a1c143dbd7d
darkwebplayer <amrithanadhm@gmail.com>
2026-02-08 10:34:08 +0530

    add gtag

--- app/app.vue
+++ app/app.vue
@@ -5,3 +5,22 @@
     </NuxtLayout>
   </div>
 </template>
+
+<script setup>
+useHead({
+  script: [
+    {
+      async: true,
+      src: 'https://www.googletagmanager.com/gtag/js?id=G-ZX94M0RZE3'
+    },
+    {
+      innerHTML: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-ZX94M0RZE3');
+      `
+    }
+  ]
+})
+</script>
